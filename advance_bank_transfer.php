e3f21609f217d3a04f2e41e9e5980ccf72cc8dd8
laxmikanttroology <40752619+laxmikanttroology@users.noreply.github.com>
2019-12-03 23:17:02 +0530

    updated files

--- advance_bank_transfer.php
+++ advance_bank_transfer.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Plugin Name: Advance Bank Transfer
- * Plugin URI: www.test.com
- * Description: Woocommerce addon payment gateway for bank transfer
+ * Plugin URI: https://wisetr.com/
+ * Description: Woocommerce addon payment gateway for bank transfer. 
  * Author: Laxmi Kant Tiwari
  * Version: 1.0.1
  *
